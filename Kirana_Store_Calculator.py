8056c43b315daeda9b4633f39c0f8a76f81ba646
Aditya raj <87028110+Adityaraj32@users.noreply.github.com>
2022-01-18 20:50:50 +0530

    Add files via upload

---
+++ Kirana_Store_Calculator.py
@@ -0,0 +1,18 @@
+'''
+Author: Adityaraj Yadav 
+Date: 18 January 2022
+Project Name: Kirana Store Calculater
+Purpose:For Practising Purpose
+'''
+
+print("\t\t\t\t\tWelcome to the Kirana Store Calculator")
+print("\t\t\t\t\tyou can exit or quit it by pressing q key")
+sum = 0
+while True:
+    User_Entry = input("Enter the price: ")
+    if User_Entry != 'q':
+        sum = sum + int(User_Entry)
+        print(f"Till now total Bill is {sum}\n")
+    else:
+        print(f"\n\tYour total Bill is {sum}. Thank you! for shopping in our shop")
+        break
